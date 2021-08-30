ecf68fd9db8a90800c63e4fb7b4a3a5101879762
ThurberDaniel <phxburg@gmail.com>
2021-08-30 16:38:19 -0600

    1st init

---
+++ Untitled-1.js
@@ -0,0 +1,12 @@
+
+var i = {};
+
+i.am = "Daniel Thurber","a software developer";
+
+i.usedTo = ["manage a team of airplanes and amazing people","help customers by listening and engaging on their pacakge delivery needs"];
+
+i.Love = ["family","3D printing","flying drones","good books","Christopher Nolan movies","macbooks"];
+
+i.workWith = ["Html","Css","JavaScript","Bootstrap","Python","Node","MySQL","mongoDB","great people"];
+
+i.aspire = "to become a key part of a highly collaborative environment and achieving team success.";
